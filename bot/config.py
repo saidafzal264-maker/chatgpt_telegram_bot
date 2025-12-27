30479489e56efef6bfdd698aa5056fd3ed3280f3
saidafzal264-maker <saidafzal264@gmail.com>
2025-12-27 12:28:34 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,17 +12,16 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
-openai_api_base = config_yaml.get("openai_api_base", None)
-allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
-new_dialog_timeout = config_yaml["new_dialog_timeout"]
-enable_message_streaming = config_yaml.get("enable_message_streaming", True)
-return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
-image_size = config_yaml.get("image_size", "512x512")
-n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
+telegram_token = "8576476633:AAFDcSvp82YIx6jwwG6uSd9pueWaDBzsbV0"
+openai_api_key = "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"
+openai_api_base = None  # agar kerak bo'lsa o'zgartiring
+allowed_telegram_usernames = ["username1", "username2"]  # misol uchun
+new_dialog_timeout = 300  # misol uchun 5 daqiqa
+enable_message_streaming = True
+return_n_generated_images = 1
+image_size = "512x512"
+n_chat_modes_per_page = 5
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
-
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
     chat_modes = yaml.safe_load(f)
